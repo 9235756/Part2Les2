bd73c37d253b52b3fe3d0633481752e2fbfeba65
9235756 <9235756@mail.ru>
2021-08-22 17:14:01 +0300

    Initial Commit

---
+++ Part2Les2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Part2Les2
+//
+//  Created by Andrei Naumenko on 22.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
